6f95f3582d8cf8bcb7b903a020dc672fa920dba1
Taisuke Fukuno <fukuno@jig.jp>
2022-09-19 08:19:19 +0900

    役所・役場の位置情報誤りの訂正
    https://github.com/code4fukui/localgovjp/issues/6

---
+++ deno/fixlatlng.js
@@ -0,0 +1,18 @@
+import { CSV } from "https://js.sabae.cc/CSV.js";
+
+const list = (await Deno.readTextFile("ll-fix.txt")).trim().split("\n");
+console.log(list);
+
+const data = CSV.toJSON(await CSV.fetch("../localgovjp-utf8.csv"));
+for (const l of list) {
+  // 02387:青森県中泊町（40.960427, 140.434122）
+  const cid = parseInt(l, 10);
+  const lg = data.find(d => d.cid == cid);
+  if (!lg) {
+    throw new Error(l);
+  }
+  const ll = l.match(/（(\d+\.\d+), (\d+\.\d+)）/);
+  lg.lat = ll[1];
+  lg.lng = ll[2];
+}
+await Deno.writeTextFile("localgovjp-utf8.csv", CSV.stringify(data));
