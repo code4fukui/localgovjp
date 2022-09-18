14b3183bcc0d6bd4d96ac8813f92017d0ea16ad4
Taisuke Fukuno <fukuno@jig.jp>
2022-09-19 08:38:35 +0900

    fix src data

--- deno/fixlatlng.js
+++ deno/fixlatlng.js
@@ -3,7 +3,7 @@ import { CSV } from "https://js.sabae.cc/CSV.js";
 const list = (await Deno.readTextFile("ll-fix.txt")).trim().split("\n");
 console.log(list);
 
-const data = CSV.toJSON(await CSV.fetch("../localgovjp-utf8.csv"));
+const data = CSV.toJSON(await CSV.fetch("c-localgovjp-utf8.csv"));
 for (const l of list) {
   // 02387:青森県中泊町（40.960427, 140.434122）
   const cid = parseInt(l, 10);
@@ -15,4 +15,4 @@ for (const l of list) {
   lg.lat = ll[1];
   lg.lng = ll[2];
 }
-await Deno.writeTextFile("localgovjp-utf8.csv", CSV.stringify(data));
+await Deno.writeTextFile("c-localgovjp-utf8.csv", CSV.stringify(data));
--- deno/make-localgov.js
+++ deno/make-localgov.js
@@ -1,3 +1,3 @@
 import { make } from "./make.js";
 
-make("localgovjp-utf8.csv", "localgovjp", "LOCALGOV_JP");
+make("c-localgovjp-utf8.csv", "localgovjp", "LOCALGOV_JP");
